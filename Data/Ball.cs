eea4f5cb8098ade34d608118bb7cc6ab44672113
dbyszewski <dbyszewski@gmail.com>
2025-03-26 19:20:19 +0100

    Init project

---
+++ Data/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPW.Data
+{
+    public class Ball
+    {
+        public Ball(float x, float y, float radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float Radius { get; private set; }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TPW.Data.Ball ball = new TPW.Data.Ball(0, 0, 10);
+            TPW.Logic.TestClass logic = new TPW.Logic.TestClass();
+            Assert.IsTrue(true);
+        }
+    }
+}
